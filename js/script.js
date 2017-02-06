9c05c1af4df4e91c5e569d96742b845c25c39489
Nicoal Price <nicoalprice@gmail.com>
2017-02-06 14:55:15 -0500

    added site optimization project and updated skills

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // JS for portfolio
+
+/* Hamburger Menu */
+$("#hamburger").on("click", function(){
+	// If menu is closed, slide down
+	// If menu is open, slide up
+	$("#menu").slideToggle();
+});
